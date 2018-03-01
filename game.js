1352b07cb5f6bdca00c0882ac948eef9560b4e06
Sumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2018-03-01 14:27:44 -0500

    Removes alert that regurgitates user's guess Adds else statement that alerts user to wrong guess

--- game.js
+++ game.js
@@ -3,9 +3,12 @@ var secretNumber = 4;
 
 //ask user for guess
 var guess = prompt("Guess a number");
-alert(guess);
 
 //check if guess is right
 if (Number(guess) === secretNumber) {
   alert("YOU GOT IT RIGHT!");
+}
+//otherwise, you got it wrong
+else {
+  alert("WRONG!");
 }
